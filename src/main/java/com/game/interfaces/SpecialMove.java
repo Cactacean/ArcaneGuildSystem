ef177d227a7db2ac78117fb10030b949a02ba103
NADIYAH AQILAH PUTRI <nadiyahhadlina01@gmail.com>
2026-01-08 17:22:02 +0800

    added method explanation

--- src/main/java/com/game/interfaces/SpecialMove.java
+++ src/main/java/com/game/interfaces/SpecialMove.java
@@ -1,7 +1,11 @@
 package com.game.interfaces;
 
 import com.game.character.Character;
-
+//Abstract Method
+    // there are NO curly braces { } and no code logic.
+    // It ends with a semicolon (;). 
+    // This tells Java: "Any class that implements SpecialMove MUST have a method 
+    // called useSpecialMove that accepts a Character as a target."
 public interface SpecialMove {
     void useSpecialMove(Character target);
 }
